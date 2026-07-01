22ec93f6f152c7f5dac3a15a66dfcd9c305daa53
Earl Co <earlvanze@gmail.com>
2026-07-01 05:28:46 +0200

    Add Codex guidance to hosted API auth errors
    Co-authored-by: Earl Co <info@earlco.in>

--- web/functions/api/funnel-event.js
+++ web/functions/api/funnel-event.js
@@ -110,6 +110,7 @@ const ALLOWED_EVENTS = new Set([
   'api_troubleshooting_key_recovery_clicked',
   'api_troubleshooting_managed_access_clicked',
   'api_troubleshooting_setup_copied',
+  'api_troubleshooting_codex_docs_clicked',
   'api_troubleshooting_oauth_clicked',
   'api_troubleshooting_oauth_failed',
   'launch_plan_viewed',
